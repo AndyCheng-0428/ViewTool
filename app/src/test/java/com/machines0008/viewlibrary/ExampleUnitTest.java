252d130a94afa577c0ac49398ce1ab177ebab8b5
Andy_Cheng <andy_cheng@turn2cloud.com>
2022-04-13 02:02:38 +0800

    廣告盒程式撰寫

---
+++ app/src/test/java/com/machines0008/viewlibrary/ExampleUnitTest.java
@@ -0,0 +1,26 @@
+package com.machines0008.viewlibrary;
+
+import com.machines0008.viewlibrary.adbox.ImageLoader;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void downloadBitmap() {
+
+    }
+}
